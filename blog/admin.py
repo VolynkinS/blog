7f6786761b9a9e68d6705b939fc92df5430068c9
Aleksandr Volynkin <sv200330@mail.ru>
2022-01-23 13:48:27 +0700

    Customized Admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Category, Tag, Post
+
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class CommonInfo(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    save_on_top = True
+
+
+class CategoryAdmin(CommonInfo):
+    pass
+
+
+class TagAdmin(CommonInfo):
+    pass
+
+
+class PostAdmin(CommonInfo):
+    form = PostAdminForm
+    list_display = ('id', 'title', 'slug', 'created_at', 'category', 'get_photo')
+    list_display_links = ('id', 'title', 'slug')
+    search_fields = ('title',)
+    list_filter = ('category',)
+    readonly_fields = ('views', 'created_at', 'get_photo')
+    fields = ('title', 'slug', 'category', 'tags', 'content', 'photo', 'get_photo', 'views', 'created_at')
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="80"')
+
+    get_photo.short_description = 'Photo'
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+'''
+Category
+==============
+title, slug
+
+Tag
+==============
+title, slug
+
+Post
+==============
+title, slug, author, content, photo, views, category, tags, created_at
+'''
+
+
+class CommonInfo(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Title')
+    slug = models.SlugField(max_length=80, unique=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        abstract = True
+        ordering = ['title']
+
+
+class Category(CommonInfo):
+    class Meta(CommonInfo.Meta):
+        verbose_name_plural = 'Categories'
+
+
+class Tag(CommonInfo):
+    class Meta(CommonInfo.Meta):
+        verbose_name_plural = 'Tags'
+
+
+class Post(CommonInfo):
+    author = models.CharField(max_length=100, verbose_name='Author')
+    content = models.TextField(verbose_name='Content')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True,
+                              verbose_name='Photo')
+    views = models.IntegerField(default=0, verbose_name='Number of views')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT,
+                                 related_name='posts', verbose_name='Category')
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts',
+                                  verbose_name='Tags')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Date of creation')
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name_plural = 'Posts'
